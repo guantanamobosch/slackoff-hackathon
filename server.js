9478b0c34bea8ee112e2601b33b42f7f8e953d33
guantanamobosch <luketyson17@gmail.com>
2023-03-10 07:52:17 -0700

    set up basic employer and employee routes

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     res.send('<h1>Hey there</h1>');
 })
+
+app.get('/home', (req, res) => {
+    res.render('home')
+})
+
+
 
 app.listen(4000, () => {
     console.log('I am listening on port 4000');
